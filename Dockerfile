f0ffdfdbd707c1763d67d4ab050c4a21f7ddf588
Sofia Lunkes <sofialunkes@gmail.com>
2022-12-04 18:11:57 -0300

    Alterando build do gradle para gerar apenas um jar

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,8 @@ FROM openjdk:8-jdk-alpine
 RUN addgroup -S revio && adduser -S revio -G revio
 USER revio:revio
 
-ARG LOCATION_JAR_FILE=build/libs/*.jar
-RUN echo $LOCATION_JAR_FILE
-ARG CONTAINER_JAR_FILE=app.jar
+ARG JAR_FILE=build/libs/*.jar
+COPY ${LOCATION_JAR_FILE} /app.jar
 
 
-COPY ${LOCATION_JAR_FILE} $CONTAINER_JAR_FILE
-
-EXPOSE 8080
-
-
-ENTRYPOINT ["java","-jar","app.jar"]
+ENTRYPOINT ["java","-jar","/app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,49 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.1"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "com.sofia"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter:2.7.5")
-	implementation("org.springframework.boot:spring-boot-starter-actuator:2.7.5")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.7.5")
-	implementation("org.springframework.boot:spring-boot-starter-validation:2.7.5")
-	implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
-	implementation("org.springframework.security:spring-security-core")
-	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-	implementation("org.passay:passay:1.6.2")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
+    implementation("org.springframework.security:spring-security-core")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("org.passay:passay:1.6.2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
-
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
